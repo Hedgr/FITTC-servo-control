c80c07dad060ceca869c0fe2c49aaf1637bd2130
Hedgr <74436143+Hedgr@users.noreply.github.com>
2022-11-06 22:02:16 -0800

    Create follower_cont.cpp

---
+++ follower_cont.cpp
@@ -0,0 +1,34 @@
+#include <Servo.h>
+
+// C++ code
+//
+
+int in0 = A0;
+int in1 = A1;
+
+Servo srv;
+int s_pin=13;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("\n\n==============================\nserial began\n");  
+  srv.attach(s_pin);
+}
+
+void loop()
+{
+  if((digitalRead(in0)==HIGH)&&(digitalRead(in1)==LOW)) {
+    srv.write(180);
+  }
+  else {
+    if((digitalRead(in0)==LOW)&&(digitalRead(in1)==LOW)) {
+      srv.write(90);
+    }
+    else {
+      if ((digitalRead(in0)==LOW)&&(digitalRead(in1)==HIGH)) {
+        srv.write(0);
+      }
+    }
+  }
+}
